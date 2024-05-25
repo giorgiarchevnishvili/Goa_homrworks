1c4687c3db5c96a34882dc28d77f84521c6d3db3
giorgiarchevnishvili <giorgiarchevnishvili@gmail.com>
2024-05-25 15:21:00 +0400

    first commit

---
+++ level 1/Homework/lesson1.py
@@ -0,0 +1,15 @@
+
+
+name = "giorgi"
+surname = "archevnishvili"
+age = 14
+height = 172
+
+print("me mqvia " + name + " " + surname + ". var " + str(age) + " wlis." + " simagle: " + str(height))
+
+
+
+
+
+
+
---
+++ level 3/homework/lesson1.py
@@ -0,0 +1,25 @@
+name = "Giorgi"
+last_name ="Archevnishvili"
+age = 14
+hobby = "watching football"
+country = "Georgia"
+
+print( name + " " +last_name +"  "+str(age) +"  "+ hobby +"  "+ country)
+
+
+
+a = "doing"
+b = "homework"
+
+print(a + " " + b)
+
+print(5+10)
+print(78-19)
+print(23*2)
+print(200//20)
+print(12/2)
+print(15**2)
+print(29*43+19)
+print(98-8+10)
+print(14//2+199)
+print(2+2*123)
