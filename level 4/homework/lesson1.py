1325e1f7ae5687aa758423fc10c074497952700a
giorgiarchevnishvili <giorgiarchevnishvili@gmail.com>
2024-05-28 21:45:49 +0400

    commit

--- level 4/homework/lesson1.py
+++ level 4/homework/lesson1.py
@@ -2,20 +2,96 @@ from turtle import *
 
 color("black")
 width(9)
-
+speed(15)
 back(290)
 
+
+
 forward(290)
+
 left(360)
 forward(290)
 
 
+right(270)
 
+forward(280)
+right(270)
+forward(190)
 right(270)
 forward(280)
-right(240)
-forward(20)
+right(90)
+forward(440)
 left(270)
+forward(280)
+left(270)
+forward(190)
+left(270)
+forward(280)
+left(180)
+forward(210)
+right(90)
+forward(245)
+right(90)
+forward(210)
+right(80)
+
+
+penup()
+goto(-80, 0)
+pendown()
+right(100)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+
+penup()
+goto(-200, -100)
+pendown()
+
+forward(200)
+left(90)
+forward(100)
+left(90)
+forward(50)
+
+
+penup()
+goto(-200, -100)
+pendown()
+
+right(90)
+forward(100)
+
+color("green")
+
+penup()
+goto(0, -100)
+pendown()
+forward(100)
+right(90)
+forward(200)
+right(90)
+forward(100)
+right(90)
+forward(200)
+
+color("black")
+
+penup()
+goto(200, -300)
+pendown()
+right(15)
+forward(200)
+right(145)
+forward(200)
+
+right(180)
+forward(100)
+left(70)
+forward(55)
 
 
 
@@ -23,15 +99,6 @@ left(270)
 
 
 
-
-
-
-
-
-
-
-
-
-
+exitonclick()
 
 
