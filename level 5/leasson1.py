4e1d97cdb109dc9a0b1d228150bc8e59a2d5d41c
giorgiarchevnishvili <giorgiarchevnishvili@gmail.com>
2024-05-28 21:43:53 +0400

    commit

---
+++ level 5/leasson1.py
@@ -0,0 +1,26 @@
+name = "giorgi"    # shevqmeni chemi saxelis cvladi
+surname = "archevnishvili"  # shevqmeni chemi gvaris cvladi
+age = 14  # shevqmeni chemi asakis cvladi
+height = 172 # shevqmeni chemi simaglis cvladi
+country = "saqartveloshi" # shevqmeni chemi qveynis cvladi
+city = "gurjaanshi"  #  shevqmeni chemi qalaqis cvladi
+print("me mqvia " + name +" " + surname + " da var " +str(age) + " wlis. vcxovrob " + country + " " + city + ". simagle " + str(height) + " cm")
+
+# cvladebi davumate ertmanets da davprinte
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
