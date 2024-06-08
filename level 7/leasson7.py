d2d76181d20b91c035e87473024d238a4dd826d2
giorgiarchevnishvili <giorgiarchevnishvili@gmail.com>
2024-06-08 19:35:09 +0400

    commit

---
+++ level 7/leasson7.py
@@ -0,0 +1,70 @@
+name = input("what is your name: ")
+last_name = input("what is your last name: ")
+age = input("what is your age: ")
+country = input("which country do you live: ")
+car = input("what is your favourite car: ")
+color = input("which color is ur favourite: ")
+print("My name is ", name, last_name, "i am ", age, "years old " "and i live in ", country, "my favourite car is ", car, "my favourite color is ", color)
+
+
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print(x + y)
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print(x - y)
+
+y = input("Enter y: ")
+print(x * y)
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print(x / y)
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print(x // y)
+
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print(x + y)
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print(x - y)
+
+y = input("Enter y: ")
+print(x * y)
+
+x = input("Enter x: ")
+y = input("Enter y: ")
+print(x / y)
+print(x // y)
+
+
+me = input("Hello, what is your name: ")
+my_future_friend = input("Hello, my name is:  ")
+me = ("my name is:" )
+my_future_friend = input("where are you from: ")
+me = input("i am from: ")
+me = input("and you?: ")
+my_future_friend = input("i am from: ")
+me = input("can we be friends: ")
+friend = input("yes: ")
+me = input("OK,i ll write you: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
