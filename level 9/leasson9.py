54e8b126dfcd2287184458e97fc3935c694333bc
giorgiarchevnishvili <giorgiarchevnishvili@gmail.com>
2024-06-12 18:38:07 +0400

    commit

---
+++ level 9/leasson9.py
@@ -0,0 +1,45 @@
+number1 = float(input("Number one: "))
+number2 = float(input("Number two: "))
+sum = number1 + number2
+print("sum:", sum)
+
+width = float(input(" width: "))
+height = float(input(" height: "))
+area = width * height
+
+print("area:", area)
+
+
+
+
+
+
+number = int(input("number: "))
+
+
+binary = bin(number)
+print("number double:", binary)
+
+
+
+
+width = float(input(" width:"))
+
+height = float(input(" height: "))
+perimeter = 2 * (width + height)
+print("p: ", perimeter)
+
+
+
+
+
+
+number = float(input(" number: "))
+
+
+square = number ** 2
+
+print("square: ", square)
+
+
+
